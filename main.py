44776685f2c517d2370feff5da7720e80f30495f
Faisal-Shahbaz <faisalsshahbaz1@gmail.com>
2025-01-27 10:59:15 -0500

    v6

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ aq = pz.Pump(f_aq, 26.59)
 org = pz.Pump(f_org, 26.59)
 master = pz.Masterppl(f_master)
 master.quickset({0: org, 1: aq})
-
 pz.Pump.init(aq, org)
+
 aq.rate(22, 20, "wdr")
 org.rate(22, 20, "wdr")
+aq.beep()
+org.beep()
 
-aq.rate(3, 20, "inf")
+aq.rate(10, 20, "inf")
+aq.rate(1,1,"wdr")
 aq.beep()
 pz.Pump.sync(aq, org)
 
-org.pause(10)
-org.rate(3, 20, "inf")
+org.pause(30)
+org.rate(3, 10, "inf")
+org.rate(10,10,"inf")
 org.beep()
-org.pause(60)
+org.pause(120)
 
 pz.Pump.sync(aq,org)
 aq.loopstart(3)
